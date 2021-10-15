a72bc8635c472357d99b1aa801b93d298885f4d1
Shivanshu Upadhyay <shivanshuu2@gmail.com>
2021-10-15 15:19:26 +0530

    Skeleton API

---
+++ API/Data/Migrations/20211014165343_schema-change.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class schemachange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Users",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "MyProperty");
+        }
+    }
+}
